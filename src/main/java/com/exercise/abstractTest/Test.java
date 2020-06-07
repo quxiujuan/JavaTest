f7a95c88796c28ac8cf40c16ad35ff676bf8b2e2
quxiujuan <quxiujuan@xiniunet.com>
2020-06-07 22:46:53 +0800

    v1

---
+++ src/main/java/com/exercise/abstractTest/Test.java
@@ -0,0 +1,11 @@
+package com.exercise.abstractTest;
+
+import javax.management.Query;
+import java.util.Iterator;
+import java.util.concurrent.*;
+
+public class MainTest {
+    public static void main(String[] args) {
+//v1
+    }
+}
