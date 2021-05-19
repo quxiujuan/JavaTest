4d78b03529bf240215f0dee59e4ee8d984150de9
quxiujuan <quxiujuan@kedacom.com>
2021-05-19 18:12:53 +0800

    从本地txt读取文件并输出到本地txt3

--- src/test/java/com/exercise/AbstractTest.java
+++ src/test/java/com/exercise/AbstractTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class AbstractTest {
     @Test
     public void testAbstract(){
-    System.out.println(2.00-1.1000);
+    System.out.println(2.00-1.10003);
 
     }
 
